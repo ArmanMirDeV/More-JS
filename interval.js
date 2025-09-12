87660c1b364556eb203289243e126e8aad6f47c8
Arman Mir <mirarman8583@gmail.com>
2025-09-12 18:42:13 +0600

    set timeOut

---
+++ interval.js
@@ -0,0 +1,17 @@
+console.log(1);
+console.log(2);
+
+let count = 0;
+ const intervalId =  setInterval(() => {
+    console.log(count++);
+    if (count > 10){
+        clearInterval(intervalId)
+    }
+
+}, 1000);
+
+console.log("Interval ID ", intervalId);
+
+console.log(4);
+console.log(5);
+console.log(6);

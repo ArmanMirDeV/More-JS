45aa2932f400bfb4efe495fc108987e2cccf6662
Arman Mir <mirarman8583@gmail.com>
2025-09-12 20:27:31 +0600

    Java Script Event loop and concurrency

---
+++ event-loop.js
@@ -0,0 +1,34 @@
+function one(){
+    two()
+    console.log(1)
+}
+function two(){
+    three()
+    console.log(2)
+}
+function three(){
+
+    setTimeout(() => {
+        console.log("Inside Three TimeOut", 0);
+    })
+
+    four()
+    console.log(3)
+}
+function four(){
+    five()
+    console.log(4)
+}
+function five(){
+      setTimeout(() => {
+        console.log("Inside Five TimeOut", 4);
+    },4000)
+    six()
+    console.log(5)
+}
+
+function six(){
+    console.log(6)
+}
+
+one()
